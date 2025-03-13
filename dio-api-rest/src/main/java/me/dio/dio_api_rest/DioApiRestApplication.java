b97e9e2771cd1e98979062335770330185051514
Zeca Monteiro <zecamonteiro991@gmail.com>
2025-03-13 18:48:36 -0300

    Adicionando projeto dio-api-rest

---
+++ dio-api-rest/src/main/java/me/dio/dio_api_rest/DioApiRestApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dio_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioApiRestApplication.class, args);
+	}
+
+}
---
+++ dio-api-rest/src/test/java/me/dio/dio_api_rest/DioApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.dio_api_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
